72b47739e19abbe58887fb271d46dbbffc816e99
Savan <patelsavan771@gmail.com>
2022-07-27 00:55:16 +0530

    basic LinkFooter structure created

---
+++ src/components/footer/FooterEnd.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FooterEnd() {
+  return (
+    <div className='footer-end'>
+        Conditions of Use &nbsp;&nbsp;  Privacy Notice &nbsp; &nbsp;&copy;2022, amazon-clone, InVi team
+    </div>
+  )
+}
