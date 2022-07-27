c80659714337d6a6c440ddad4d967f3727ed9ba6
Savan <patelsavan771@gmail.com>
2022-07-27 20:26:22 +0530

    deleted comment

--- src/components/RowContainer.js
+++ src/components/RowContainer.js
@@ -9,31 +9,7 @@ export default function RowContainer(props) {
             <h3 className='p-2'>{props.heading}</h3>
 
             <div className='d-flex overflow-auto'>
-                {props.imgUrls.map((ele) => 
-                <RowImg src={ele} alt="" />)}
-            {/* <RowImg src={process.env.PUBLIC_URL + '/images/books/book1.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book2.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book3.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book4.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book5.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book6.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book7.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book8.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book9.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book10.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book2.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book1.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book2.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book3.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book4.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book5.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book6.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book7.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book8.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book9.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book10.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book2.jpg'} alt="" />
-            <RowImg src={process.env.PUBLIC_URL + '/images/books/book3.jpg'} alt="" /> */}
+                {props.imgUrls.map((ele) => <RowImg src={ele} alt="" />)}
         </div>
         </div>
         
