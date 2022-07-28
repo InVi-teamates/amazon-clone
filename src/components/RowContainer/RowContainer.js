8625a47a3aef5f30c534a157650a4fd2ba6c5306
hoaiglx <hoainv@galaxy.com.vn>
2022-07-29 00:02:45 +0700

    update scrollbbar

--- src/components/RowContainer/RowContainer.js
+++ src/components/RowContainer/RowContainer.js
@@ -7,11 +7,15 @@ export default function RowContainer(props) {
     
     return (
         <div className="row-container container mt-3">
-            <h3 className='p-2'>{props.heading}</h3>
+            {[1,2,3].map((ele) =>
+            <div className="row-item">
+                <h3 className='p-2'>{props.heading}</h3>
 
-            <div className='d-flex overflow-auto'>
-                {props.imgUrls.map((ele) => <RowImg src={ele} alt="" />)}
-            </div>
+                <div className='d-flex overflow-overlay row-scrollbar'>
+                    {props.imgUrls.map((ele) => <RowImg src={ele} alt="" />)}
+                </div>
+            </div>)
+            }
         </div> 
     )
 }
